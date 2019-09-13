82d546b41f9e065e2286600205dfecd2b4b1e582
Marlie Shirley <marliecshirley@gmail.com>
2019-09-13 13:38:29 -0500

    finished assessment

---
+++ assessment4.js
@@ -0,0 +1,69 @@
+//1. Create a for loop to iterate through the provided array. If an element is divisible by 3, console log the result of the number divided by 3.
+
+function divide(){
+    var array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+    var divide = 3;
+    for(divide of array){
+        divide += array;
+}
+    console.log(divide)
+}
+divide();
+
+//2. Create a while loop that will run while the value of counter is less than 100. For each iteration, it should print out "The value of counter is: " followed by the value of counter. 1 should also be added to the counter during each iteration.
+
+var counter = 0;
+while(counter < 100){
+    console.log("The value of the counter is :" + counter = 25)
+}
+
+//3. Create a function called subtractNumbers that takes in two numbers as input. The function should print out the difference between the two numbers.
+var num1 = 4
+var num2 = 7
+function subtractNumbers (numbers){
+    var input = num1 && num2
+    console.log("Difference between the numbers are: " + (num1 -num2));
+}
+
+//4. Create a function that takes an array of strings as input. The function should return the shortest of the strings as output.
+
+function countLetters(string){
+    var count =0
+    if(letter ===4 || letter ===8 || letter ===12){
+        count ++;
+    }
+
+}
+return count;
+console.log(countLetters("This is a string"));
+
+//5. Create a function called calculateLetterGrade that takes in an assignment grade as input. The function should use conditional logic to return the letter grade corresponding to the assignment grade, as indicated by the following scale:
+
+function calculateLetterGrade(){
+    if(A === 90-100 || B === 80-89 || C === 70-79 || D === 60-69 || F === < 60){
+        console.log(calculateLetterGrade);
+    }
+}
+/*
+    A = 90-100
+    B = 80-89
+    C = 70-79
+    D = 60-69
+    F = < 60
+*/
+
+//BONUS: Create a function that takes in 3 numbers as input. The function should return true if all three of the numbers are less than 10 and the numbers multiplied together is less than 30.
+
+//BONUS: Create a function that iterates through the numbers 0 to 100 that plays the Fizzbuzz game. 
+
+/* 
+    FizzBuzz rules:
+        * If the number is divisible by both 3 and 5, print "FizzBuzz"
+        * If the number is divisible by 3, print "Fizz"
+        * If the number is divisible by 5, print "Buzz"
+
+*/
+
+//WHEN YOU ARE DONE WITH THIS ASSESSMENT: CREATE A LOCAL REPOSITORY, COMMIT YOUR FILES, AND CREATE A GITHUB REPOSITORY AND PUSH THEM UP!
+
+
